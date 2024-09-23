1ab268f4b8b7c956e107fb9b1341e58ab33f0796
men100 <men100per@gmail.com>
2024-09-23 15:14:30 +0900

    mtk3bsp2_mqtt_sample: separate task

--- mtk3bsp2_mqtt_sample/STM32CubeIDE/Application/User/app_main.c
+++ mtk3bsp2_mqtt_sample/STM32CubeIDE/Application/User/app_main.c
@@ -2,6 +2,7 @@
 #include <tk/tkernel.h>
 #include <tm/tmonitor.h>
 #include "lwip/timeouts.h"
+#include "main.h"
 #include "ethernetif.h"
 #include "app_ethernet.h"
 #include "mtk3bsp2_mqtt.h"
@@ -9,71 +10,50 @@
 const char* Mqtt_Topic = "sample";
 
 extern struct netif gnetif;
-BOOL isSubscribe = FALSE;
 BOOL isPublish = FALSE;
 
-LOCAL void task_1(INT stacd, void *exinf);	// task execution function
-LOCAL ID	tskid_1;			// Task ID number
-LOCAL T_CTSK ctsk_1 = {				// Task creation information
+LOCAL void task_handler(INT stacd, void *exinf);	// task execution function
+LOCAL ID	tskid_handler;	  // Task ID number
+LOCAL T_CTSK ctsk_handler = { // Task creation information
 	.itskpri	= 10,
 	.stksz		= 1024,
-	.task		= task_1,
+	.task		= task_handler,
 	.tskatr		= TA_HLNG | TA_RNG3,
 };
 
-LOCAL void task_2(INT stacd, void *exinf);	// task execution function
-LOCAL ID	tskid_2;			// Task ID number
-LOCAL T_CTSK ctsk_2 = {				// Task creation information
+LOCAL void task_publisher(INT stacd, void *exinf);	// task execution function
+LOCAL ID	tskid_publisher;	// Task ID number
+LOCAL T_CTSK ctsk_publisher = {	// Task creation information
 	.itskpri	= 10,
 	.stksz		= 1024,
-	.task		= task_2,
+	.task		= task_publisher,
 	.tskatr		= TA_HLNG | TA_RNG3,
 };
-
-LOCAL void task_1(INT stacd, void *exinf)
-{
-	while(1) {
-		tm_printf((UB*)"task 1\n");
-
-		/* Inverts the LED on the board. */
-		out_w(GPIO_ODR(B), (in_w(GPIO_ODR(B)))^((1<<0)|(1<<14)));
-		out_w(GPIO_ODR(E), (in_w(GPIO_ODR(E)))^(1<<1));
-
-		tk_dly_tsk(500);
-	}
-}
-
-LOCAL void task_2(INT stacd, void *exinf)
-{
-	while(1) {
-		tm_printf((UB*)"task 2\n");
-		tk_dly_tsk(700);
-	}
-}
 
 LOCAL void mtk3bsp2_mqtt_subscribe_request_cb(void *arg, int result) {
   if (result != ERR_OK) {
 	tm_printf((UB*)"mtk3bsp2_mqtt_subscribe_request_cb: subscribe failed(%d)\n", result);
+	BSP_LED_Off(LED1);
   } else {
 	tm_printf((UB*)"mtk3bsp2_mqtt_subscribe_request_cb: subscribe successfully\n");
-	isSubscribe = TRUE;
+	BSP_LED_On(LED1);
   }
 }
 
 LOCAL void mtk3bsp2_mqtt_publish_request_cb(void *arg, int result) {
   if (result != ERR_OK) {
 	tm_printf((UB*)"mtk3bsp2_mqtt_publish_request_cb: publish failed(%d)\n", result);
-	isPublish = FALSE;
   } else {
 	tm_printf((UB*)"mtk3bsp2_mqtt_publish_request_cb: published successfully\n");
   }
+  isPublish = FALSE;
 }
 
 LOCAL void mtk3bsp2_mqtt_connection_cb(void* arg, mtk3bsp2_mqtt_connection_status_t status) {
 	if (status == MTK3BSP2_MQTT_CONNECT_ACCEPTED) {
 	    tm_printf((UB*)"mtk3bsp2_mqtt_connection_cb: MQTT client connected successfully\n");
 
-	    // サブスクライブ
+	    // Subscribe
         mtk3bsp2_mqtt_subscribe(Mqtt_Topic, 0, mtk3bsp2_mqtt_subscribe_request_cb, NULL);
 	} else {
 		tm_printf((UB*)"mtk3bsp2_mqtt_connection_cb: MQTT client connection failed(%d)\n", status);
@@ -93,27 +73,56 @@ LOCAL void mtk3bsp2_mqtt_incoming_data_cb(void *arg, const u8_t *data, u16_t len
   }
 }
 
+LOCAL void task_handler(INT stacd, void *exinf)
+{
+	while(1) {
+    	// Read a received packet from the Ethernet buffers and send it to the lwIP for handling
+    	ethernetif_input(&gnetif);
+
+		// Handle timeouts
+		sys_check_timeouts();
+
+#if LWIP_NETIF_LINK_CALLBACK
+		Ethernet_Link_Periodic_Handle(&gnetif);
+#endif
+
+#if LWIP_DHCP
+		DHCP_Periodic_Handle(&gnetif);
+#endif
+		tk_dly_tsk(100);
+	}
+}
+
+LOCAL void task_publisher(INT stacd, void *exinf)
+{
+	char message[80];
+	int count = 0;
+	while(1) {
+
+    	if (!isPublish) {
+			u8_t qos = 0;
+			u8_t retain = 0;
+
+			sprintf(message, "Hello, mtk3bsp2_mqtt!: Count=%d\n", ++count);
+
+    		// Publish
+			err_t err = mtk3bsp2_mqtt_publish(Mqtt_Topic, message, strlen(message), qos, retain, mtk3bsp2_mqtt_publish_request_cb, NULL);
+			if (err != ERR_OK) {
+				tm_printf((UB*)"mtk3bsp2_mqtt_connection_cb: mqtt_publish failed: %d\n", err);
+			}
+			isPublish = TRUE;
+    	}
+
+		tk_dly_tsk(3000);
+	}
+}
+
 /* usermain関数 */
 EXPORT INT usermain(void)
 {
 	int ret = 0;
 
 	tm_putstring((UB*)"Start User-main program.\n");
-
-	// Turn off the LED on the board.
-	/*
-	out_w(GPIO_ODR(B), (in_w(GPIO_ODR(B)))&~((1<<0)|(1<<14)));
-	out_w(GPIO_ODR(E), (in_w(GPIO_ODR(E)))&~(1<<1));
-
-	// Create & Start Tasks
-	tskid_1 = tk_cre_tsk(&ctsk_1);
-	tk_sta_tsk(tskid_1, 0);
-
-	tskid_2 = tk_cre_tsk(&ctsk_2);
-	tk_sta_tsk(tskid_2, 0);
-
-	tk_slp_tsk(TMO_FEVR);
-	*/
 
 	ret = mtk3bsp2_mqtt_init(192, 168, 1, 72);
 	if (ret != 0) {
@@ -129,35 +138,13 @@ EXPORT INT usermain(void)
 
 	mtk3bsp2_mqtt_set_inpub_callback(mtk3bsp2_mqtt_incoming_publish_cb, mtk3bsp2_mqtt_incoming_data_cb, NULL);
 
-    while (1) {
-    	if (isSubscribe && !isPublish) {
-    		// パブリッシュ
-			const char* topic = "sample";
-			const char* message = "Hello, MQTT!";
-			u8_t qos = 0;
-			u8_t retain = 0;
+	tskid_handler = tk_cre_tsk(&ctsk_handler);
+	tk_sta_tsk(tskid_handler, 0);
 
-			err_t err = mtk3bsp2_mqtt_publish(topic, message, strlen(message), qos, retain, mtk3bsp2_mqtt_publish_request_cb, NULL);
-			if (err != ERR_OK) {
-				tm_printf((UB*)"mtk3bsp2_mqtt_connection_cb: mqtt_publish failed: %d\n", err);
-			}
-			isPublish = TRUE;
-    	}
+	tskid_publisher = tk_cre_tsk(&ctsk_publisher);
+	tk_sta_tsk(tskid_publisher, 0);
 
-    	// Read a received packet from the Ethernet buffers and send it to the lwIP for handling
-    	ethernetif_input(&gnetif);
-
-		// Handle timeouts
-		sys_check_timeouts();
-
-#if LWIP_NETIF_LINK_CALLBACK
-		Ethernet_Link_Periodic_Handle(&gnetif);
-#endif
-
-#if LWIP_DHCP
-		DHCP_Periodic_Handle(&gnetif);
-#endif
-    }
+	tk_slp_tsk(TMO_FEVR);
 
 	return 0;
 }
