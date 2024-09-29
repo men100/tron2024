3f046695ae0b6809cc4a771b9366cfe79bbcd9ed
men100 <men100per@gmail.com>
2024-09-30 00:06:32 +0900

    mtk3bsp2_mqtt_ids_sign: Add publisher and cycle checker

--- mtk3bsp2_mqtt_ids_sign/STM32CubeIDE/Application/User/app_main.c
+++ mtk3bsp2_mqtt_ids_sign/STM32CubeIDE/Application/User/app_main.c
@@ -11,6 +11,7 @@
 #define FLAG_USER_BUTTON		0x01
 #define FLAG_MQTT_DISCONNECTED	0x02
 #define FLAG_DATA_RECEIVED		0x04
+#define FLAG_PERIODIC_REQUEST	0x08
 
 extern UART_HandleTypeDef huart2;
 extern struct netif gnetif;
@@ -21,13 +22,34 @@ const char* Mqtt_Broker_Ip_Addr = "192.168.1.72";
 // MQTT Client ID (ユニークである必要がある)
 const char* Mqtt_Client_Id = "ids_subscriber";
 
-// MQTT Topic
-const char* Mqtt_Topic = "ids";
+// MQTT Topic (情報配信用)
+const char* Mqtt_Topic_Ids = "ids";
+
+// MQTT Topic (情報要求用)
+const char* Mqtt_Topic_Request = "request";
 
 // MQTT Broker に接続されているかどうか
 BOOL is_mqtt_connected = FALSE;
 
+// MQTT Publish 中かどうか
+BOOL is_mqtt_publishing = FALSE;
+
+// 周期リクエストのしきい値 (ms)
+const int Periodic_Request_Threshold = 120000;
+
+// 周期リクエストの発行間隔 (ms)
+const int Periodic_Request_Interval = 60000;
+
+// 前回 ECR-1264 が更新された時刻
+SYSTIM periodic_request_start;
+
+// 周期ハンドラの起動周期 (ms)
+const int Cyc_Checker_Tim = 30000;
+
+// Subscribe で受信中のデータの offset。受信するたびに増えていく
 int ti_data_offset = 0;
+
+// MQTT Message で受信するデータの総サイズ
 int total_ti_data_size = 0;
 
 LOCAL ID	flgid;
@@ -55,13 +77,34 @@ LOCAL T_CTSK ctsk_receiver = {	// Task creation information
 	.tskatr		= TA_HLNG | TA_RNG3,
 };
 
+LOCAL void task_publisher(INT stacd, void *exinf);	// task execution function
+LOCAL ID	tskid_publisher;	// Task ID number
+LOCAL T_CTSK ctsk_publisher = {	// Task creation information
+	.itskpri	= 10,
+	.stksz		= 1024,
+	.task		= task_publisher,
+	.tskatr		= TA_HLNG | TA_RNG3,
+};
+
+LOCAL void cyc_checker(void* exinf);	// Cycle handler execution funcion
+LOCAL ID	cycid_checker; // Cyc ID number
+LOCAL T_CCYC ccyc_checker = {	// Cycle handler creation information
+	.cycatr		= TA_HLNG,
+	.cychdr		= cyc_checker,
+	.cyctim		= Cyc_Checker_Tim,
+	.cycphs		= 0
+};
+
 // EXTI検出コールバック
 void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) {
 	// User Button が押された
 	if (GPIO_Pin == B1_Pin) {
-    }
+		tk_set_flg(flgid, FLAG_USER_BUTTON);
+		periodic_request_start.lo += Periodic_Request_Interval;
+	}
 }
 
+// Subscribe Request の結果を返す Callback
 LOCAL void mtk3bsp2_mqtt_subscribe_request_cb(void *arg, int result) {
   if (result != ERR_OK) {
 	tm_printf((UB*)"mtk3bsp2_mqtt_subscribe_request_cb: subscribe failed(%d)\n", result);
@@ -72,13 +115,22 @@ LOCAL void mtk3bsp2_mqtt_subscribe_request_cb(void *arg, int result) {
   }
 }
 
+// Publish Request の結果を返す Callback
+LOCAL void mtk3bsp2_mqtt_publish_request_cb(void *arg, int result) {
+  if (result != ERR_OK) {
+	tm_printf((UB*)"mtk3bsp2_mqtt_publish_request_cb: publish failed(%d)\n", result);
+  }
+  is_mqtt_publishing = FALSE;
+}
+
+// Connection の状態を返す Callback
 LOCAL void mtk3bsp2_mqtt_connection_cb(void* arg, mtk3bsp2_mqtt_connection_status_t status) {
 	if (status == MTK3BSP2_MQTT_CONNECT_ACCEPTED) {
 	    tm_printf((UB*)"mtk3bsp2_mqtt_connection_cb: MQTT client connected successfully\n");
 	    is_mqtt_connected = TRUE;
 
 	    // Subscribe
-        mtk3bsp2_mqtt_subscribe(Mqtt_Topic, 0, mtk3bsp2_mqtt_subscribe_request_cb, NULL);
+        mtk3bsp2_mqtt_subscribe(Mqtt_Topic_Ids, 0, mtk3bsp2_mqtt_subscribe_request_cb, NULL);
 	} else {
 		tm_printf((UB*)"mtk3bsp2_mqtt_connection_cb: MQTT client connection failed(%d)\n", status);
 		is_mqtt_connected = FALSE;
@@ -103,6 +155,7 @@ LOCAL void mtk3bsp2_mqtt_incoming_data_cb(void *arg, const u8_t *data, u16_t len
   }
 }
 
+// パケットを受信する Task
 LOCAL void task_handler(INT stacd, void *exinf)
 {
 	while(1) {
@@ -138,10 +191,61 @@ LOCAL void task_receiver(INT stacd, void *exinf)
 	    	ecr1264_update();
 
 	    	tm_printf((UB*)"[task_receiver] updated ECR-1264.\n");
+
+	    	// ECR-1264 が更新されたので、周期リクエストの base も更新
+	    	tk_get_utc(&periodic_request_start);
 	    } else {
 	    	tm_printf((UB*)"[task_receiver] received length is incorrect.(received=%d, total_length=%d)\n",
 	    			ti_data_offset, total_ti_data_size);
 	    }
+	}
+}
+
+// Publish する Task
+LOCAL void task_publisher(INT stacd, void *exinf)
+{
+	while(1) {
+		tk_wai_flg(flgid, FLAG_USER_BUTTON | FLAG_PERIODIC_REQUEST, TWF_ORW | TWF_BITCLR, NULL, TMO_FEVR);
+
+		tm_printf((UB*)"[task_publisher] awaked.\n");
+
+    	if (is_mqtt_connected && !is_mqtt_publishing) {
+    		const char* message = "Please send me to latest information";
+    		u8_t qos = 0;
+			u8_t retain = 0;
+
+    		// Publish
+			err_t err = mtk3bsp2_mqtt_publish(Mqtt_Topic_Request, message, strlen(message), qos, retain, mtk3bsp2_mqtt_publish_request_cb, NULL);
+			if (err != ERR_OK) {
+				tm_printf((UB*)"mtk3bsp2_mqtt_connection_cb: mqtt_publish failed: %d\n", err);
+			} else {
+				tm_printf((UB*)"[task_publisher] published.\n");
+			}
+			is_mqtt_publishing = TRUE;
+    	} else {
+			tm_printf((UB*)"[task_publisher] not published. (is_mqtt_connected=%s, is_mqttt_publishing).\n",
+					is_mqtt_connected ? "true" : "false", is_mqtt_publishing ? "true" : "false");
+    	}
+	}
+}
+
+// 前回の更新からの時刻を計算し、指定時間を超えていた場合は task_publisher に Publish を促す
+LOCAL void cyc_checker(void* exinf)
+{
+	SYSTIM now;
+	UW diff;
+
+	tk_get_utc(&now);
+	diff = now.lo - periodic_request_start.lo;
+
+	tm_printf((UB*)"[cyc_checker] elapsed time since last update = %d ms.\n", diff);
+
+	if (!is_mqtt_publishing && diff >= Periodic_Request_Threshold) {
+		tm_printf((UB*)"[cyc_checker] Thresholds were exceeded, so set flag.\n");
+		tk_set_flg(flgid, FLAG_PERIODIC_REQUEST);
+
+		// 連続して Publish し続けないように間隔をあける
+		periodic_request_start.lo += Periodic_Request_Interval;
 	}
 }
 
@@ -152,6 +256,7 @@ EXPORT INT usermain(void)
 
 	tm_putstring((UB*)"Start mtk3bsp2_mqtt_ids_sign.\n");
 
+	tk_get_utc(&periodic_request_start);
 	ecr1264_init(&huart2);
 
 	ret = mtk3bsp2_mqtt_init(Mqtt_Broker_Ip_Addr);
@@ -175,6 +280,12 @@ EXPORT INT usermain(void)
 
 	tskid_receiver = tk_cre_tsk(&ctsk_receiver);
 	tk_sta_tsk(tskid_receiver, 0);
+
+	tskid_publisher = tk_cre_tsk(&ctsk_publisher);
+	tk_sta_tsk(tskid_publisher, 0);
+
+	cycid_checker = tk_cre_cyc(&ccyc_checker);
+	tk_sta_cyc(cycid_checker);
 
 	tk_slp_tsk(TMO_FEVR);
 
